6d319e5c2d45dd498f25ffdb12204a3c29ffdbbd
jerome (jblb) <jerome@jblb.net>
2014-01-25 16:08:45 +0100

    doc interupt

--- my_def.h
+++ my_def.h
@@ -3,3 +3,5 @@
 // definition des pins et de leur usage
 
 const int led_pin = 13;			// default to pin 13
+const int out_left = 4;			// output1 to pin 4
+const int out_right = 5;			// output2 to pin 13
--- my_interupt.cc
+++ my_interupt.cc
@@ -6,10 +6,28 @@
 #include <Arduino.h>
 #include "my_def.h"
 
+static byte 	Address;		// Board adress as XEEEAAAA
+							// X undef
+							// EEE tean adress
+							// AAAA board adress
+								
+static byte 	Data;		// data as XXXXXXdd
+							//
+/*
+ * pour chaque bit dans address + data
+ * 	si il est a 1 set soritie et temps = 8 (8*5 = 40 ms)
+ *  si il est a 0 set sortie et temps = 4 (4*5=20ms)
+ * 	si temp de bit = 0 reset sortie et temps = 1 (5ms)
+ * 
+ * si fin de trame temps = 1 (5ms)
+ * 
+ * 							
+
+
 void inter5()
 {
 static boolean output = HIGH;
-  
+
   digitalWrite(led_pin, output);
   output = !output;
 }
